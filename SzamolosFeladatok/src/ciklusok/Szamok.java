031723a57cb760437cfd236ee2592afb9caee72a
Hidy97 <csaladi.alexandra@gmail.com>
2023-03-03 12:35:01 +0100

    Számok class létrehozása és ciklussal kiiratás

---
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -0,0 +1,11 @@
+package ciklusok;
+public class Szamok {
+    public static void main(String[] args) {
+        while(int j = 2; j < 3; j++) {
+            for (int i = 2; i < 4; i++) {
+                System.out.println(j, i);
+            }
+        }
+        
+    }
+}
