159c73005b177153f6fc3081b373728c1c1c5b66
Hidy97 <csaladi.alexandra@gmail.com>
2023-03-03 12:40:39 +0100

    JAVÍTVA: Számok class létrehozása és ciklussal kiiratás

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -1,9 +1,9 @@
 package ciklusok;
 public class Szamok {
     public static void main(String[] args) {
-        while(int j = 2; j < 3; j++) {
-            for (int i = 2; i < 4; i++) {
-                System.out.println(j, i);
+        for (int j = 2; j < 4; j++) {
+            for (int i = 2; i < 5; i++) {
+                System.out.println(i);
             }
         }
         
